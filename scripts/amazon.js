8cc8c5f81ae061227b4f9d732531428449a7a02b
Sik~Dokta <musyokibenedict01@gmail.com>
2024-11-05 13:53:50 +0300

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,6 +81,13 @@ if (productId == item.productId){
         quantity: 1
       });
     }
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity') 
+    .innerHTML = cartQuantity;
+    
   });
 });
